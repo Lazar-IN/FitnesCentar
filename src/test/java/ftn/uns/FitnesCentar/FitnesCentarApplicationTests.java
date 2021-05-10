aa6221d6070b796de36051671bbcbbc42e840c61
Lazar-IN <lazar.so.025@gmail.com>
2021-05-10 11:05:59 +0200

    Inicijalni comit

---
+++ src/test/java/ftn/uns/FitnesCentar/FitnesCentarApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.uns.FitnesCentar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesCentarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
